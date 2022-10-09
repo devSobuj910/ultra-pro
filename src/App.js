3cacb7c3471f810172eb3635053395ecec6a6b49
devSobuj910 <ftsobuj222@gmail.com>
2022-10-10 02:56:36 +0600

    reset react fille accidently

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import "./App.css";
-import Header from "./component/Headers/Header";
-import Catagory from "./component/Catagory/Catagory";
-import User from "./component/User/User";
-import Breks from "./component/Breeks/Breks";
-import Details from "./component/Ex-detils/Details";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="left-side ms-5 col-8  ">
-          <Header></Header>
-          <Catagory></Catagory>
-        </div>
-        <div className="sidbar bg-danger col-3 ">
-          <User></User>
-          <Breks></Breks>
-          <Details></Details>
-        </div>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
